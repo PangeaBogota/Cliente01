ed57227b7364260254e41449560e487bf4ddd990
PangeaBogota <Pangeabogota@hotmail.com>
2016-08-08 13:50:15 -0500

    Revert "a"
    This reverts commit 0958f34b3b77b34d5072c789b23f4b76c75697f2.

--- controller/ventasController.js
+++ controller/ventasController.js
@@ -554,7 +554,6 @@ app_angular.controller("pedidoController",['Conexion','$scope','$location','$htt
 			
 		});
 		$scope.item=item;
-		$scope.Variables=[];
 		$scope.Variables.descripcion=item.observaciones;
 		$scope.onChangeComboItem();
 		//console.log(index)
