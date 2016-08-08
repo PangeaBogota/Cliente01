a7863a95729359a5223816399f1b2f2de05d85a3
PangeaBogota <Pangeabogota@hotmail.com>
2016-08-08 13:50:52 -0500

    Revert "cambios21313123"
    This reverts commit 0721046d606e04e6f3b7c524138a84122d1165ed.

--- js/app.js
+++ js/app.js
@@ -49,7 +49,7 @@ app_angular.controller('sessionController',['bootbox','Conexion','$scope','$loca
     $scope.$watch('online', function(newStatus) 
         //$scope.status.connextionstate==false
         {$scope.status.connextionstate=newStatus;  
-            if ($scope.status.connextionstate==false) {
+            if (true) {
             $scope.alerta.message='Verifique su conexion a Internet ';
             $scope.alerta.disableBtnAceptar=false;
             $scope.alerta.header='Conexion Internet'
